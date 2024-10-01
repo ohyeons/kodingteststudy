3d177922f745622fdd293ba4e36ecf3a43045892
ohyeons <eksldpf01@naver.com>
2024-10-02 01:50:52 +0900

    문제26 각도기

---
+++ week2/practice7_2.py
@@ -0,0 +1,12 @@
+#각도기
+
+def solution(angle):
+    if angle>0 and angle<90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif angle == 180:
+        answer = 4
+    else:
+        answer = 3
+    return answer
