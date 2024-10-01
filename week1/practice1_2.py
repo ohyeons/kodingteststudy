bd0c9ac71cfa3fc1e6eba473efe468536af2f83a
ohyeons <eksldpf01@naver.com>
2024-10-02 00:15:11 +0900

    문제25 특정 문자제거

--- week1/practice1_2.py
+++ week1/practice1_2.py
@@ -3,3 +3,4 @@
 def solution(num1, num2):
     answer = num1-num2
     return answer
+    print(solution)
---
+++ week2/practice7_1.py
@@ -0,0 +1,5 @@
+#특정 문자제거
+
+def solution(my_string, letter):
+    answer = my_string.replace(letter,"")
+    return answer
