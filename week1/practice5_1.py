c1f8084822b8f95b654ab34b6392dfce9e9415b4
ohyeons <eksldpf01@naver.com>
2024-10-01 21:37:26 +0900

    문제17 옷가게 할인 받기

---
+++ week1/practice5_1.py
@@ -0,0 +1,12 @@
+#옷가게 할인 받기
+
+def solution(price):
+    if price >= 500000:
+        answer = price*0.8
+    elif price >= 300000:
+        answer = price*0.9
+    elif price >= 100000:
+        answer = price*0.95
+    else:
+        answer = price
+    return int(answer)
