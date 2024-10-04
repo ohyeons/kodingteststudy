2d13c676dc8b1b21daa52cba0a2a8f83101b4611
ohyeons <eksldpf01@naver.com>
2024-10-04 12:49:52 +0900

    문제32 순서쌍의 개수

---
+++ week2/practice8_4.py
@@ -0,0 +1,11 @@
+#순서쌍의 개수
+
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        j = n%i
+        if j==0:
+            answer+=1
+        else:
+            continue
+    return answer
