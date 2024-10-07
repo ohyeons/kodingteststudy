ed2d4c3a702948ec8c1e6ae9f9c6aa0e25140208
ohyeons <eksldpf01@naver.com>
2024-10-07 15:43:30 +0900

    문제40 배열 회전시키기

---
+++ week2/practice10_4.py
@@ -0,0 +1,10 @@
+#배열 회전시키기
+
+def solution(numbers, direction):
+    answer = []
+    if direction == 'right':
+        answer = numbers[-1:]+numbers[:-1]
+    elif direction == 'left':
+        answer = numbers[1:]+numbers[:1]
+            
+    return answer
