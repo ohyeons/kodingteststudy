c6dfe7b8dd024364fb530966a4c97dd2add27953
ohyeons <eksldpf01@naver.com>
2024-11-05 17:11:30 +0900

    문제97 문자열 밀기

---
+++ week3/practice25_1.py
@@ -0,0 +1,11 @@
+#문자열 밀기
+
+def solution(A, B):
+    answer = -1
+    if A==B:
+        return 0
+    for i in range(1, len(A)) :
+        A = A[-1]+A[0:-1]
+        if A == B:
+            return i  
+    return answer
