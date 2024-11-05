646ab7617a59031b009b8ccbfce11ae6a271af8c
ohyeons <eksldpf01@naver.com>
2024-11-05 17:06:43 +0900

    문제89 특이한 정렬

---
+++ week3/practice23_1.py
@@ -0,0 +1,10 @@
+#특이한 정렬
+
+def solution(numlist, n):
+    answer = []
+    for i in range(10000):
+        if(n+i in numlist):
+            answer.append(n+i)
+        if(i!=0 and n-i in numlist):
+            answer.append(n-i)
+    return answer
